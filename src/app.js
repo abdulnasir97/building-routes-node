7c4b325482f5582fd0e6974f9121c2230e2d1c2a
Abdul M Nasir <moeid.nasir@gmail.com>
2023-03-08 04:45:29 -0500

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const { NODE_ENV = "development" } = process.env;
+const express = require("express");
+const app = express();
+
+app.get("/ping", (req, res) => {
+  res.send("pong!");
+});
+
+app.get("/welcome", (req, res) => {
+  res.send("Welcome to my server.");
+});
+
+module.exports = app;
